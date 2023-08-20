d56c474866a4019e1b642383ce70ff156a0836bf
Husseini <mhmd.husseinyy@gmail.com>
2023-08-20 18:17:00 +0300

    displaying all recipes

--- src/components/pages/Landing/index.jsx
+++ src/components/pages/Landing/index.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import RecipeCard from '../../RecipeCard';
+import { sendRequest } from '../../../config/request';
+import { Link } from 'react-router-dom'; 
+import "./style.css";
 
 const Landing = () => {
-  return (
-    <div>Landingggg</div>
-  )
-}
+  const [recipes, setRecipes] = useState([]);
 
-export default Landing
+  const fetchRecipes = async () => {
+    try {
+      const response = await sendRequest({ route: "/getRecipe", body: "" });
+      setRecipes(response);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }; 
+
+  useEffect(() => {
+    fetchRecipes();
+  }, []); 
+
+  return (
+    <div className="recipe-list">
+      {recipes.map(recipe => (
+        <Link key={recipe.id} to={`/landing/${recipe.id}`}>
+        <RecipeCard recipe={recipe} />
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Landing;
